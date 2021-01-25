8926394860ace0c835f308cd07b15bc12f7245a0
hieufix1710 <hieufix1710@gmail.com>
2021-01-25 09:59:25 +0700

    chapter6

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title page_title = ""
-base_title = "Ruby on Rails Tutorial Sample App"
-page_title.empty? ? base_title : "#{page_title} | #{base_title}"
-end
+    base_title = I18n.t "ruby_on_rails_tutorial_sample_app"
+    page_title.empty? ? base_title : "#{page_title} | #{base_title}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "static_pages/home"
+  get "static_pages/help"
+  scope "(:locale)", locale: /en|vi/ do
+    resources :microposts
+    resources :users
+  end
 end
