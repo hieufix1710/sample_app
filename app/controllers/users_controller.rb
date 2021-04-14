b8ff16fb563c2458c7bed33463451a98b83016fb
mvn-hieunguyen2-dn <hieu.nguyen2@monstar-lab.com>
2021-04-14 14:24:07 +0700

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
     if @user.save
       @user.send_activation_email
       flash[:info] = "Please check your email to activate your account."
-      redirect_to login_path
+      redirect_to login_url
     else
       render :new
     end
