8f0dbb5858eefa04e6d918e03dd94b9b644ad818
mvn-hieunguyen2-dn <hieu.nguyen2@monstar-lab.com>
2021-04-14 11:04:32 +0700

    up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -124,4 +124,19 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = {host: "localhost:3000" }
+    config.action_mailer.perform_deliveries = true
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "gmail.com",
+  user_name: ENV["MAIL_USERNAME"],
+  password: ENV["MAIL_PASSWORD"],
+  authentication: "plain",
+  enable_starttls_auto: true
+}
 end
