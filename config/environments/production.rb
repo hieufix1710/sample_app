671f7f9eedb839731b1277c11963b8256de573f9
mvn-hieunguyen2-dn <hieu.nguyen2@monstar-lab.com>
2021-04-15 16:25:01 +0700

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -133,7 +133,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
-  domain: "damp-peak-65043.herokuapp.com",
+  domain: "gmail.com",
   user_name: ENV["MAIL_USERNAME"],
   password: ENV["MAIL_PASSWORD"],
   authentication: "plain",
